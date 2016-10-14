ddea4410f1f21a92416c3f77c71e9b8a3b3d26ad
Julian Trajanson <julian.trajanson@gmail.com>
2016-10-14 03:09:48 -0400

    finished binary tree

--- Chapter 11/BinaryTree.hs
+++ Chapter 11/BinaryTree.hs
@@ -31,3 +31,70 @@ mapOkay =
   if mapTree (+1) testTree' == mapExpected
   then print "yup okay!"
   else error "test failed!"
+
+
+
+preorder :: BinaryTree a -> [a]
+preorder Leaf = []
+preorder (Node left center right) = [center]
+                                  ++ (preorder left)
+                                  ++ (preorder right)
+
+inorder :: BinaryTree a -> [a]
+inorder Leaf = []
+inorder (Node left center right) = (inorder left)
+                                  ++ [center]
+                                  ++ (inorder right)
+
+postorder :: BinaryTree a -> [a]
+postorder Leaf = []
+postorder (Node left center right) = (postorder left)
+                                  ++ (postorder right)
+                                  ++ [center]
+
+testTree :: BinaryTree Integer
+testTree = Node (Node Leaf 1 Leaf) 2 (Node Leaf 3 Leaf)
+
+testPreorder :: IO ()
+testPreorder =
+  if preorder testTree == [2, 1, 3]
+  then putStrLn "Preorder fine!"
+  else putStrLn "Bad news bears."
+
+testInorder :: IO ()
+testInorder =
+  if inorder testTree == [1, 2, 3]
+  then putStrLn "Inorder fine!"
+  else putStrLn "Bad news bears."
+
+testPostorder :: IO ()
+testPostorder =
+  if postorder testTree == [1, 3, 2]
+  then putStrLn "Postorder fine!"
+  else putStrLn "Bad news bears."
+
+
+
+foldTree :: (a -> b -> b) -> b -> BinaryTree a -> b
+foldTree f startVal tree = foldr f startVal (inorder tree)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+--
