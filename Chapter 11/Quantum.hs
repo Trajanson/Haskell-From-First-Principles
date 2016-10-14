07d25d8277ce289db12f18a9b9767d87935b8fd4
Julian Trajanson <julian.trajanson@gmail.com>
2016-10-14 02:16:49 -0400

    finish quantum

---
+++ Chapter 11/Quantum.hs
@@ -0,0 +1,86 @@
+module Quantum
+
+where Quantum = Yes
+              | No
+              | Both
+              deriving (Eq, Show)
+
+
+-- 3 + 3 (SUM)
+--------------------
+
+quantSum1 :: Either Quantum Quantum
+quantSum1 = Right Yes
+
+quantSum2 :: Either Quantum Quantum
+quantSum2 = Right No
+
+quantSum3 :: Either Quantum Quantum
+quantSum3 = Right Both
+
+quantSum4 :: Either Quantum Quantum
+quantSum4 = Left Yes
+
+quantSum5 :: Either Quantum Quantum
+quantSum5 = Left No
+
+quantSum6 :: Either Quantum Quantum
+quantSum6 = Left Both
+
+
+
+
+-- 3 * 3 (PRODUCT)
+--------------------
+quantProd1 :: (Quantum, Quantum)
+quantProd1 = (Yes, Yes)
+
+quantProd2 :: (Quantum, Quantum)
+quantProd2 = (Yes, No)
+
+quantProd3 :: (Quantum, Quantum)
+quantProd3 = (Yes, Both)
+
+quantProd4 :: (Quantum, Quantum)
+quantProd4 = (No, Yes)
+
+quantProd5 :: (Quantum, Quantum)
+quantProd5 = (No, No)
+
+quantProd6 :: (Quantum, Quantum)
+quantProd6 = (No, Both)
+
+quantProd7 :: (Quantum, Quantum)
+quantProd7 = (Both, Yes)
+
+quantProd8 :: (Quantum, Quantum)
+quantProd8 = (Both, No)
+
+quantProd9 :: (Quantum, Quantum)
+quantProd9 = (Both, Both)
+
+
+
+
+
+
+
+-- 3 ^ 3 (exponentiation for ->)
+--------------------
+
+quantFlip1 :: Quantum -> Quantum
+quantFlip1 Yes  = Yes
+quantFlip1 No   = Yes
+quantFlip1 Both = Yes
+
+quantFlip2 :: Quantum -> Quantum
+quantFlip2 Yes  = Yes
+quantFlip2 No   = Yes
+quantFlip2 Both = No
+
+quantFlip3 :: Quantum -> Quantum
+quantFlip3 Yes  = Yes
+quantFlip3 No   = Yes
+quantFlip3 Both = Both
+
+-- ...
