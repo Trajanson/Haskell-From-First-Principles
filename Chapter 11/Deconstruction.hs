f98a93541be83649a6328e34cc1384300e6e5a80
Julian Trajanson <julian.trajanson@gmail.com>
2016-10-14 02:06:12 -0400

    Quantum

---
+++ Chapter 11/Deconstruction.hs
@@ -0,0 +1,28 @@
+module Deconstruction where
+
+newtype Name  = Name String deriving Show
+
+newtype Acres = Acres Int deriving Show
+
+
+data FarmerType = DairyFarmer
+                | WheatFarmer
+                | SoybeanFarmer
+                deriving (Show)
+
+data Farmer = Farmer Name Acres FarmerType deriving Show
+
+
+isDairyFarmer :: Farmer -> Bool
+isDairyFarmer (Farmer _ _ DairyFarmer) = True
+
+
+data FarmerRec = FarmerRec { name       :: Name
+                           , acres      :: Acres
+                           , farmerType :: FarmerType }
+                           deriving Show
+
+isDairyFarmerRec :: FarmerRec -> Bool
+isDairyFarmerRec farmer = case farmerType farmer of
+    DairyFarmer -> True
+    _           -> False
